140a624256b0d8df5d6b5f86003c3b1185445df9
Sugarwoodrop <135331725+Sugarwoodrop@users.noreply.github.com>
2025-12-06 23:23:39 +0700

    Add byte swapping and binary representation functions

---
+++ Lab1/4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define SUCCES 0
+#define ERROR -1
+
+void ToBitInt(int number){
+    int count =0;
+    for(int i =(int)sizeof(int) * 8 - 1; i >= 0; i--){
+        if(count == 8){
+            printf(" ");
+            count = 0;
+        }
+        if( ((number>>i) & 1) == 0){
+            printf("0");
+            count++;
+            continue;
+        }
+        printf("1");
+        count++;
+    }
+    printf("\n");
+}
+
+void SwapByte(int numberFirst, int numberSecond){
+    ToBitInt(numberFirst);
+    numberFirst &= ~(0xFF << 16);
+    numberFirst |= numberSecond << 16;
+    ToBitInt(numberFirst);
+}
+
+int main(){
+    int numberFirst;
+    int numberSecond;
+
+    scanf("%d", &numberFirst);
+    scanf("%d", &numberSecond);
+
+    if(numberSecond > 255 || numberSecond < 0){
+        printf("Bad number\n");
+        return ERROR;
+    }
+    SwapByte(numberFirst, numberSecond);
+    
+    return SUCCES;
+}
